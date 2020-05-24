d43090add144b23e99791b47fd44065eab2f4f82
Kshitija27-c <65860094+Kshitija27-c@users.noreply.github.com>
2020-05-24 20:24:13 +0530

    Add files via upload

---
+++ style.php
@@ -0,0 +1,46 @@
+<style type="text/css" media="screen">
+	*{margin: 0; padding: 0; box-sizing: border-box;font-family: 'Roboto', sans-serif;}
+
+	.nav_style{
+		background-color: #766663
+	}
+
+	.nav_style a{color: white
+	}
+
+/*////////main header///////*/
+.main_header{
+	height: 450px;
+	width: 100%;
+}
+.rightside h1{
+	font-size: 3rem; text-align: center; text-rendering:  
+}
+.leftside img{
+	animation: unity 3s linear infinite
+}
+@keyframes unity{
+	0% { transform: scale(0.40); }
+	50% { transform: scale(0.65); }
+	100% { transform: scale(0.85); }	
+}
+.corona_update{
+	margin: 0 0 30px 0;
+}
+.corona_update h3{
+	color: 150502; font-size: 3rem;
+}
+.corona_update h1{ font-size: 2rem; text-align: center; }
+
+.sub_section{
+	background-color: #fbfafd
+}
+.footer_style{
+	background-color: #766663
+}
+.footer_style p{
+	margin-bottom: 0;
+} 
+
+
+</style>
